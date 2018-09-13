6fdcefd62becac270356c2dc3873d06294efa978
bryanmagers <41593930+bryanmagers@users.noreply.github.com>
2018-09-13 11:38:04 -0700

    timer function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,25 @@
 $(document).ready(function() {
 
+    function timer(seconds) {
+        var interval = setInterval(function() {          
+            if (seconds === 0) {
+                $("#timer").text("Time's up!");
+                clearInterval(interval);
+            } else {
+                $("#timer").text(seconds + " seconds left");
+                seconds--;
+            }
+        }, 1000);
+    }
+
     $.ajax({
         url: "https://opentdb.com/api.php?amount=10&type=multiple",
         method: "GET"
     }).then(function(response) {
         var questions = response.results;
-        $("#question").html(questions[0].question);
+        var category = $("<p>").html("Category: " + questions[0].category);
+        var question = $("<h2>").html(questions[0].question);
+        $("#question").append(category).append(question);
         // right answer
         var correctAnswer = $("<div class='answer' id='correct'>").html(questions[0].correct_answer);
 
@@ -18,7 +32,7 @@ $(document).ready(function() {
         });
 
 
-
+        timer(15);
 
 
 
