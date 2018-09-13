a4dd50cda96a1f381810961998970d0e5c42752c
bryanmagers <41593930+bryanmagers@users.noreply.github.com>
2018-09-13 16:16:08 -0700

    timeouts and intervals function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,40 +1,81 @@
 $(document).ready(function() {
 
-    function timer(seconds) {
-        var interval = setInterval(function() {          
-            if (seconds === 0) {
-                $("#timer").text("Time's up!");
-                clearInterval(interval);
-            } else {
-                $("#timer").text(seconds + " seconds left");
-                seconds--;
-            }
-        }, 1000);
-    }
+    var current = 0;
+    var rightAnswers = 0;
+    var wrongAnswers = 0;
+    var timeoutAnswers = 0;
+    var interval;
+    var secondsLeft;
 
     $.ajax({
         url: "https://opentdb.com/api.php?amount=10&type=multiple",
         method: "GET"
     }).then(function(response) {
         var questions = response.results;
-        var category = $("<p>").html("Category: " + questions[0].category);
-        var question = $("<h2>").html(questions[0].question);
-        $("#question").append(category).append(question);
-        // right answer
-        var correctAnswer = $("<div class='answer' id='correct'>").html(questions[0].correct_answer);
+        console.log(questions);
 
-        $("#question").append(correctAnswer);
+        function askQuestion() {
+
+            if (current < 10) {
+
+                secondsLeft = 15;
+
+                interval = setInterval(function() {     
+                    if (secondsLeft === 0) {
+                        timeoutAnswers++;
+                        $("#timer").text("Time's up!");
+                        clearInterval(interval);
+                        current++;
+                        askQuestion();
+                    } else {
+                        $("#timer").text(secondsLeft + " seconds left");
+                        secondsLeft--;
+                    }
+                }, 1000);
+
+                var category = $("<p>").html("Category: " + questions[current].category);
+                var question = $("<h2>").html(questions[current].question);
+                $("#question").empty().append(category).append(question);
         
-        var incorrectAnswers = questions[0].incorrect_answers;
-        $.each(incorrectAnswers, function(key, value) {
-            var incorrectAnswerDiv = $("<div class='answer'>").html(value);
-            $("#question").append(incorrectAnswerDiv);
-        });
+                var answers = questions[current].incorrect_answers;
+                var correctAnswer = questions[current].correct_answer;
+                answers.push(correctAnswer);
+                answers.sort();
+                $.each(answers, function(index, value) {
+                    var newDiv = $("<div>").addClass("answer").html(value);
+                    if (value === correctAnswer) {
+                        newDiv.attr("id", "correctAnswer");
+                        console.log("correct answer is " + value);
+                    }
+                    $("#question").append(newDiv);
+                });
+                $(".answer").on("click", function() {
+                    clearInterval(interval);
+                    $(".answer").off("click"); 
+                    if (this.id === "correctAnswer") {
+                        rightAnswers++;
+                        $("#timer").text("RIGHT");
+                        $(this).addClass("correctAnswer");
+                    } else {
+                        wrongAnswers++;
+                        $("#timer").text("WRONG");
+                        $(this).addClass("incorrectAnswer");
+                        setTimeout(function() {
+                            $("#correctAnswer").addClass("correctAnswer");
+                        }, 2000);             
+                    }
+                    setTimeout(function() {
+                        current++;
+                        askQuestion();
+                    }, 4000);
+                });
+            } else {
+                $("#timer").text("No more questions.");
+            }
 
+        }
 
-        timer(15);
-
-
+        askQuestion();
 
     });
 
